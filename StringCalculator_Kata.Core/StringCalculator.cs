0fcd39956631c25359b40305fb9ea17b042c8bfb
pere.frontera@fdsa.es <Shithills123#>
2019-03-29 12:29:35 +0100

    Template

--- StringCalculator_Kata.Core/StringCalculator.cs
+++ StringCalculator_Kata.Core/StringCalculator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.InteropServices;
-
+﻿
 namespace StringCalculator_Kata.Core
 {
     public static class StringCalculator
@@ -10,41 +6,6 @@ namespace StringCalculator_Kata.Core
         public static string HelloWorld()
         {
             return "Hello World";
-        }
-
-        public static int Add(string numbers)
-        {
-            if (string.IsNullOrEmpty(numbers)) return 0;
-
-            var splitedNumbers = numbers
-                .Split(GetDefaultDelimiter(numbers),
-                    StringSplitOptions.RemoveEmptyEntries);
-
-            
-            var parsed_numbers = new int[splitedNumbers.Length];
-            for (var x = 0; x < parsed_numbers.Length; x++)
-            {
-                int.TryParse(splitedNumbers[x], out var a);
-                parsed_numbers[x] = a;
-            }
-
-            return parsed_numbers.Sum();
-        }
-
-        public static bool ContainsDefaultDelimiter(string numbers)
-        {
-            if (numbers[0] == '/' && numbers[1] == '/' && numbers[3] == '\n')
-            {
-                return true;
-            }
-
-            return false;
-        }
-        
-        public static char[] GetDefaultDelimiter(string numbers)
-        {
-            return ContainsDefaultDelimiter(numbers) ? new char[] {numbers[2]} : new char[]{',', '\n'};
-        }
-  
+        }  
     }
 }
